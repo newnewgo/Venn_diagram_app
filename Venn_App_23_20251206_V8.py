47fce06063afa8e1e7c33fe45843d5681a74595f
newnewgo <49686133+newnewgo@users.noreply.github.com>
2025-12-06 12:46:19 +0800

    Add files via upload

--- Venn_App_23_20251206_V8.py
+++ Venn_App_23_20251206_V8.py
@@ -6,6 +6,11 @@
 
 import streamlit as st
 import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']  # Windows
+# plt.rcParams['font.sans-serif'] = ['AppleGothic']       # macOS
+# plt.rcParams['font.sans-serif'] = ['Noto Sans CJK JP'] # Linux/其他系統
+plt.rcParams['axes.unicode_minus'] = False  # 負號正確顯示
+
 from matplotlib_venn import venn2, venn3
 
 # 顏色混合函式（RGB 加權平均）
